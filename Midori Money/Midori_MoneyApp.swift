3e10ec4901441628e8239fde21b57a35d7458fa2
Takurus <developer@takurus.com>
2022-06-27 13:22:35 -0400

    Initial Commit

---
+++ Midori Money/Midori_MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Midori_MoneyApp.swift
+//  Midori Money
+//
+//  Created by Jack Farray on 2022-06-13.
+//
+
+import SwiftUI
+
+@main
+struct Midori_MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
